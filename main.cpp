e3c0d17eee211927baf4e85bd91a00efb9882f2d
motz-a-ball <memaza98@gmail.com>
2025-09-04 21:08:06 +0000

    I have coded the program

--- main.cpp
+++ main.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 
 int main() {
- 
+    int height = 61;
+    int weight = 130;
+    
+ double kg;
+ double meters;
+//calculations for bmi//
+kg = weight * 0.453592; //multiplies weight by the conversion for kg to lbs//
+meters = height * 0.0254;
 
+// calculate bmi//
+
+double bmi;
+bmi = kg/(meters * meters);
+
+cout <<"The BMI for this person is : " << bmi << endl;
 
 }
